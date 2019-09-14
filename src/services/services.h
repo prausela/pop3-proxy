f6546b88a51de11e354566e34a0d74a4fceb7833
Gaston Lifschitz <gaslif@gmail.com>
2019-09-14 16:27:17 -0300

    Adding services such as transforming dns to ip and other services

---
+++ src/services/services.h
@@ -0,0 +1,7 @@
+#ifndef SERVICES_H_ACK0Q8X1WrbMI9ZFlPxsr98iOM
+#define SERVICES_H_ACK0Q8X1WrbMI9ZFlPxsr98iOM
+
+int is_valid_ip_address(char *ipAddress);
+int hostname_to_ip(char * hostname, char *ip);
+
+#endif
---
+++ src/services/validate_ip.c
@@ -0,0 +1,16 @@
+#include <stdio.h> //printf
+#include <string.h> //memset
+#include <stdlib.h> //for exit(0);
+#include <sys/socket.h>
+#include <errno.h> //For errno - the error number
+#include <netdb.h>	//hostent
+#include <arpa/inet.h>
+#include <stdbool.h>
+#include "services.h"
+
+int is_valid_ip_address(char *ipAddress)
+{
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+    return result != 0;
+}
