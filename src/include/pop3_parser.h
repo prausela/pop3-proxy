961b38baa1df80fd4834080df013d8900dc51147
Micaela Banfi <mbanfi@itba.edu.ar>
2019-09-15 19:20:58 -0300

    fixed some problems

--- src/include/pop3_parser.h
+++ src/include/pop3_parser.h
@@ -1,6 +1,8 @@
 #ifndef POP3_PARSER_H_ACK0Q8X1WrbMI9ZFlPxsr98iOM
 #define POP3_PARSER_H_ACK0Q8X1WrbMI9ZFlPxsr98iOM
-
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 
 struct parser *
