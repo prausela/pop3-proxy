d57369c62cda4c3216069d6223bae2992fcfb946
ubuntu <ubuntu@localhost.com>
2019-10-27 17:04:47 -0300

    Encryption file and test that removes /r/n from /r/n./r/n

---
+++ src/include/pop3_encrypt_parser.h
@@ -0,0 +1,15 @@
+#ifndef POP3_ENCRYPT_PARSER_H
+#define POP3_ENCRYPT_PARSER_H
+
+#include "parser_utils.h"
+
+struct parser *pop3_encrypt_parser_init(void);
+
+enum pop3_encrypt_event_type
+{
+	//IGNORE =0,
+	DAT_STUFFED = 10,
+	DAT_STUFFED_END,
+};
+
+#endif
---
+++ src/testing/pop3_encrypt_parser_test.c
@@ -0,0 +1,32 @@
+#include "../include/pop3_encrypt_parser.h"
+#include "../services/services.h"
+#include<stdio.h>
+#include<string.h>
+#include <stdlib.h>
+
+//test
+
+#define TRUE 1
+#define FALSE 0
+
+inline
+static char* get_event_type(unsigned type){
+	switch(type){
+		case IGNORE:
+			return "IGNORE";
+		case DAT_STUFFED:
+			return "DAT_STUFFED";
+		case DAT_STUFFED_END:
+			return "DAT_STUFFED_END";
+		default:
+			return NULL;
+	}
+}
+
+int main(int argc, char *argv[]){
+    char *to_analyse = "HOLA \r\n. como\r\n..\r\nEstas\r\n.\rYo Bien\r\n.\r\n\nPEPE";
+    char * encrypted_message = malloc(strlen(to_analyse) + 1);
+    encrypt(to_analyse,encrypted_message);
+    printf("%s",encrypted_message);
+
+}
