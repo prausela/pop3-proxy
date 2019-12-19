f8227238bff35e610d5608d4208f45b92f78e629
Paula Oseroff <poseroff@itba.edu.ar>
2019-12-19 20:13:31 -0300

    Resolves socket addresses

--- src/pop3filter/main.c
+++ src/pop3filter/main.c
@@ -10,6 +10,7 @@
  * DNS utilizando getaddrinfo), pero toda esa complejidad está oculta en
  * el selector.
  */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -22,15 +23,23 @@
 #include <sys/socket.h>  // socket
 #include <netinet/in.h>
 #include <netinet/tcp.h>
+#include <netdb.h>
 
 #include "services/pop3/include/pop3_suscriptor.h"
 #include "utils/include/lib.h"
 #include "services/pop3/nio/include/pop3_client_nio.h"
 
+#define FILTER_DEFAULT_IPV4_ADDRESS	INADDR_ANY
+#define FILTER_DEFAULT_IPV6_ADDRESS	in6addr_any
+#define FILTER_DEFAULT_PORT 		1110
+#define CTL_DEFAULT_ADDRESS			((in_addr_t) 16777343) //inet_addr("127.0.0.1")
+#define CTL_DEFAULT_PORT			9090
+
 static void
 sigterm_handler(const int signal) {
 	printf("signal %d, cleaning up and exiting\n",signal);
 	done = true;
+	exit(0);
 }
 
 /*
@@ -56,118 +65,70 @@ param_validation(int argc,char **argv,char* proxy_address, char* proxy_address_i
 
 static
 int
-init_socket(const char * port, const char * address, const char **err_msg, int protocol, int is_origin){
-	struct sockaddr_in addr;
-	struct sockaddr_in6 addr_ipv6;
-	struct sockaddr_in addr_ipv4_aux;
-	memset(&addr, 0, sizeof(addr));
-	memset(&addr_ipv4_aux, 0, sizeof(addr_ipv4_aux));
-	memset(&addr_ipv6, 0, sizeof(addr_ipv6));
-	int server;
-	int ans;
-	int ip=0;
+init_socket(struct addrinfo *addrinfo, unsigned port, const char **err_msg){
+	const int server = socket(addrinfo->ai_family, addrinfo->ai_socktype, addrinfo->ai_protocol);
 
-	if(is_origin){
-		printf("Creando origin server... la direccion es: %s\n",address );
-		if(inet_pton(AF_INET, address, &(addr.sin_addr)))
-		{
-			printf("Es una direccion IPV4\n");
-			addr_ipv4_aux.sin_family      = AF_INET;
-			addr_ipv4_aux.sin_addr.s_addr = inet_addr(address);
-			addr_ipv4_aux.sin_port        = htons(atoi(port));
-			//fprintf(stderr,"Valor 1: %d\n",addr_ipv4_aux.sin_port);
-			//fprintf(stderr,"Valor 2: %d\n",addr_ipv4_aux.sin_addr.s_addr);
-			init_curr_origin_server(req_addrtype_ipv4,NULL, addr_ipv4_aux.sin_port,addr_ipv4_aux,addr_ipv6);
-		}
-		else if(inet_pton(AF_INET6, address, &(addr_ipv6.sin6_addr))){
-			printf("Es una direccion IPV6\n");
-			addr_ipv6.sin6_family      = AF_INET6;
-			addr_ipv6.sin6_addr		   = in6addr_any;
-			addr_ipv6.sin6_port        = htons(atoi(port));
-			init_curr_origin_server(req_addrtype_ipv6,NULL,addr_ipv6.sin6_port,addr,addr_ipv6);
-		}
-		else{
-			addr.sin_family      = AF_UNSPEC;
-			addr.sin_addr.s_addr = inet_addr(address);
-			addr.sin_port        = htons(atoi(port));
-			init_curr_origin_server(req_addrtype_domain,address,addr.sin_port,addr,addr_ipv6);
-		}
+	union sockaddr_in_any {
+		struct sockaddr_in 	any;
+		struct sockaddr_in6 ipv6;
+	} addr;
+
+	switch (addrinfo->ai_family)
+	{
+		case AF_INET6:
+			memset(&addr.ipv6, 0, sizeof(addr.ipv6));
+			addr.ipv6.sin6_family      	= addrinfo->ai_family;
+			addr.ipv6.sin6_addr 		= ((struct sockaddr_in6*)addrinfo->ai_addr)->sin6_addr;
+			addr.ipv6.sin6_port        	= htons(port);
+			break;
+		
+		default:
+			memset(&addr.any, 0, sizeof(addr.any));
+			addr.any.sin_family      	= addrinfo->ai_family;
+			addr.any.sin_addr.s_addr	= *((uint32_t*) & (((struct sockaddr_in*)addrinfo->ai_addr)->sin_addr));
+			addr.any.sin_port        	= htons(port);
+			break;
 	}
+    
+    if(server < 0) {
+        err_msg = "unable to create socket";
+        return -1;
+    }
 
+    //fprintf(stdout, "Listening on TCP port %d\n", port);
 
-	else{
-		if(inet_pton(AF_INET, address, &(addr.sin_addr)))
-		{
-			ip=AF_INET;
-			printf("Reconocio que la direccion pasada es IPV4: %s\n", address);
-			addr.sin_family      = AF_INET;
-			addr.sin_addr.s_addr = inet_addr(address);
-			addr.sin_port        = htons(atoi(port));
-			server = socket(AF_INET, SOCK_STREAM, protocol);
-			if(server < 0) {
-				*err_msg = "unable to create socket";
-				return server;
-			}
+    // man 7 ip. no importa reportar nada si falla.
+    setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
 
-		}else if(inet_pton(AF_INET6, address, &(addr_ipv6.sin6_addr))){
-			ip=AF_INET6;
-			printf("Reconocio que la direccion pasada es IPV6: %s\n",address );
-			server = socket(AF_INET6, SOCK_STREAM, 0);
-			if(server < 0) {
-				*err_msg = "unable to create socket";
-				return server;
-			}
-			//printf("El valor de server es: %d\n",server );
-			addr_ipv6.sin6_family      = AF_INET6;
-			//addr_ipv6.sin6_addr		   = in6addr_any;
-			addr_ipv6.sin6_port        = htons(atoi(port));
-			}
-			else {
-				printf("Esto no deberia mostrarse (ejecucion de nombre)\n");
-			//Resolucion del nombre
-			addr.sin_family      = AF_UNSPEC;
-			addr.sin_addr.s_addr = inet_addr(address);
-			addr.sin_port        = htons(atoi(port));
-			server = socket(AF_UNSPEC, SOCK_STREAM, protocol);
-			if(server < 0) {
-				*err_msg = "unable to create socket";
-				return server;
-			}
-			}
-	}
+    if(bind(server, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+        err_msg = "unable to bind socket";
+        return -1;
+    }
 
-	/////////////////////////////
-
-	if(protocol == IPPROTO_TCP){
-		fprintf(stdout, "Listening on TCP port %s\n", port);
-	}
-	else if(protocol == IPPROTO_SCTP){
-		fprintf(stdout, "Listening on SCTP port %s\n", port);
-	}
-	// man 7 ip. no importa reportar nada si falla.
-	setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
-
-	if(ip==AF_INET6){
-		printf("About to bind IPV6....\n");
-		if((ans = bind(server, (struct sockaddr*) &addr_ipv6, sizeof(addr_ipv6))) < 0) {
-			*err_msg = "unable to bind socket";
-			return ans;
-		}
-	}
-	else if(ip==AF_INET){
-		printf("About to bind IPV4....\n");
-		if((ans = bind(server, (struct sockaddr*) &addr, sizeof(addr))) < 0) {
-			*err_msg = "unable to bind socket";
-			return ans;
-		}
-	}
-
-	if ((ans = listen(server, 20)) < 0) {
-		*err_msg = "unable to listen";
-		return ans;
-	}
-
+    if (listen(server, 20) < 0) {
+        err_msg = "unable to listen";
+        return -1;
+    }
 	return server;
+}
+
+static
+int
+init_socket_with_addrinfo(const char* host, int family, unsigned port, struct addrinfo *hints, struct addrinfo **addrinfo, char **err_msg){
+	
+	hints->ai_family 	= family;
+	
+	int errcode = getaddrinfo(host, NULL, hints, addrinfo);
+	
+	if(errcode != 0){
+		perror("getaddrinfo");
+		return -1;
+	} else {
+		const int server = init_socket(*addrinfo, port, err_msg);
+		freeaddrinfo(*addrinfo);
+		return server;
+	}
+	return -1;
 }
 
 int
@@ -189,67 +150,110 @@ print_err_msg(selector_status ss, const char **err_msg){
 int
 main(const int argc, const char **argv) {
 	int 		ret  	 = 0;
-	unsigned 	port 	 = 1100;
-	char 		proxy_address[100];
-	char 		proxy_address_ipv6[100];
-	char 		client_port[100];
-	char 		admin_address[100];
-	char 		admin_address_ipv6[100];
-	char 		admin_port[100];
-	char 		origin_address[100];
-	char 		origin_port[100];
-
-
-	strcpy(proxy_address, "0.0.0.0");
-	strcpy(proxy_address_ipv6, "::1");
-	strcpy(client_port, "1110");
-	strcpy(admin_address, "127.0.0.1");
-	strcpy(admin_address_ipv6, "::1");
-	strcpy(admin_port, "9090");
-	strcpy(origin_address, "127.0.0.1");
-	strcpy(origin_port, "110");
-	//201.212.7.96
-
-	if((ret = param_validation(argc, argv, proxy_address, proxy_address_ipv6, client_port, admin_address, admin_address_ipv6, admin_port, origin_address, origin_port)) != 0){
-		return ret;
-	}
-
-	if(strcmp(origin_address, "localhost")==0){
-		if((strcmp(origin_port, client_port))==0){
-			printf("-ERR. Proxy server cannot match origin server\n");
-			return 1;
-		}
-	}
-
 
 	// No input from user is needed
 	close(STDIN_FILENO);
 
 	// Variable for error messages
 	const char	*err_msg 	= NULL;
-	const int 	server 	 	= init_socket(client_port, proxy_address, &err_msg, IPPROTO_TCP,0);
-	const int 	server_ipv6 = init_socket(client_port, proxy_address_ipv6, &err_msg, IPPROTO_TCP,0);
+	char * host = "localhost";
+	struct addrinfo hints, *addrinfo;
 
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_socktype 	= SOCK_STREAM;
+	hints.ai_flags 		|= AI_CANONNAME;
 
-	//printf("Despues de init socket, server tiene el valor de: %d\n",server );
-	if(server < 0){
-		//printf("server menor a cero\n" );
-		goto finally;
+	hints.ai_protocol	= IPPROTO_TCP;
+
+	const int server_ipv4 = init_socket_with_addrinfo(host, AF_INET, FILTER_DEFAULT_PORT, &hints, &addrinfo, &err_msg);
+	const int server_ipv6 = init_socket_with_addrinfo(host, AF_INET6, FILTER_DEFAULT_PORT, &hints, &addrinfo, &err_msg);
+	
+	hints.ai_protocol	= IPPROTO_SCTP;
+
+	const int admin_ipv4 = init_socket_with_addrinfo(host, AF_INET, CTL_DEFAULT_PORT, &hints, &addrinfo, &err_msg);
+	const int admin_ipv6 = init_socket_with_addrinfo(host, AF_INET6, CTL_DEFAULT_PORT, &hints, &addrinfo, &err_msg);
+	/*
+	hints.ai_family 	= AF_INET;
+
+	int errcode = getaddrinfo(host, NULL, &hints, &addrinfo);
+
+	if(errcode != 0){
+		perror("getaddrinfo");
+		//return -1;
+	} else {
+		const int server_ipv4 = init_socket(addrinfo, FILTER_DEFAULT_PORT, err_msg);
+		freeaddrinfo(addrinfo);
 	}
+
+	hints.ai_family 	= AF_INET6;
+
+	errcode = getaddrinfo(host, NULL, &hints, &addrinfo);
+
+	if(errcode != 0){
+		perror("getaddrinfo");
+		//return -1;
+	} else {
+		const int server_ipv6 = init_socket(addrinfo, FILTER_DEFAULT_PORT, err_msg);
+		freeaddrinfo(addrinfo);
+	}
+	
+	hints.ai_protocol	= IPPROTO_SCTP;
+
+	hints.ai_family 	= AF_INET;
+
+	errcode = getaddrinfo(host, NULL, &hints, &addrinfo);
+
+	if(errcode != 0){
+		perror("getaddrinfo");
+		//return -1;
+	} else {
+		const int server_ipv4 = init_socket(addrinfo, CTL_DEFAULT_PORT, err_msg);
+		if(server_ipv4 == -1){
+			printf("%s\n", err_msg);
+		}
+		freeaddrinfo(addrinfo);
+	}
+
+	hints.ai_family 	= AF_INET6;
+
+	errcode = getaddrinfo(host, NULL, &hints, &addrinfo);
+
+	if(errcode != 0){
+		perror("getaddrinfo");
+		//return -1;
+	} else {
+		const int server_ipv6 = init_socket(addrinfo, CTL_DEFAULT_PORT, err_msg);
+		if(server_ipv6 == -1){
+			printf("%s\n", err_msg);
+		}
+		freeaddrinfo(addrinfo);
+	}
+
+	*/
+	/*const int 	server_ipv4 	 	= init_socket(FILTER_DEFAULT_PORT, FILTER_DEFAULT_IPV4_ADDRESS, &err_msg, IPPROTO_TCP, AF_INET);
+	const int 	server_ipv6 	 	= init_socket(FILTER_DEFAULT_PORT, FILTER_DEFAULT_IPV6_ADDRESS, &err_msg, IPPROTO_TCP, AF_INET6);
+	//const int 	server_ipv6 = init_socket(client_port, proxy_address_ipv6, &err_msg, IPPROTO_TCP,0);
+
+*/
+	//printf("Despues de init socket, server tiene el valor de: %d\n",server );
+	//if(server < 0){
+		//printf("server menor a cero\n" );
+		//goto finally;
+	// }
 
 	//printf("casi admin\n" );
-	const int 	admin 	 	= init_socket(admin_port, admin_address, &err_msg, IPPROTO_SCTP,0);
-	const int 	admin_ipv6 	= init_socket(admin_port, admin_address_ipv6, &err_msg, IPPROTO_SCTP,0);
+	//const int 	admin 	 	= init_socket(admin_port, admin_address, &err_msg, IPPROTO_SCTP,0);
+	//const int 	admin_ipv6 	= init_socket(admin_port, admin_address_ipv6, &err_msg, IPPROTO_SCTP,0);
   //Valor 1
-	if(admin < 0){
-		goto finally;
-	}
+	//if(admin < 0){
+	//	goto finally;
+	//}
 
-	const int 	origin 	 = init_socket(origin_port, origin_address, &err_msg, IPPROTO_SCTP,1);
+	//const int 	origin 	 = init_socket(origin_port, origin_address, &err_msg, IPPROTO_SCTP,1);
 	//Valor 1
-	if(admin < 0){
-		goto finally;
-	}
+	//if(admin < 0){
+	//	goto finally;
+	//}
 
 
 
@@ -258,7 +262,11 @@ main(const int argc, const char **argv) {
 	signal(SIGTERM, sigterm_handler);
 	signal(SIGINT,  sigterm_handler);
 
-	if(selector_fd_set_nio(server) == -1) {
+	while(1){
+
+	}
+
+	/*if(selector_fd_set_nio(server) == -1) {
 		err_msg = "getting server socket flags";
 		goto finally;
 	}
@@ -272,5 +280,6 @@ finally:
 	if(server >= 0) {
 		close(server);
 	}
+	*/
 	return ret;
 }
